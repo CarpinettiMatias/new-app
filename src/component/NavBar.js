88b8abcc1f3345ab35af14bf0dd9c3e2f8f23ec5
CarpinettiMatias <carpi417@gmail.com>
2021-11-02 20:39:09 -0300

    agregando desplegable
    navbar

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar'
-import { Nav } from 'react-bootstrap'
+import { Nav, NavDropdown } from 'react-bootstrap'
+
 import img from './moon-icon.png'
 
 
@@ -14,6 +15,12 @@ const NavBar = () => {
                     </Navbar.Brand>
                         <Nav className="mr-auto">
                         <Nav.Link >Home</Nav.Link>
+                        <NavDropdown title="Product" id="basic-nav-dropdown">
+                            <NavDropdown.Item >Case</NavDropdown.Item>
+                            <NavDropdown.Item >Speaker</NavDropdown.Item>
+                            <NavDropdown.Item >novelties</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                        </NavDropdown>
                         <Nav.Link >Contact</Nav.Link>
                         <Nav.Link >About</Nav.Link>
                     </Nav>
