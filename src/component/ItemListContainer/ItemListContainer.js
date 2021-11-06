6aaaf8347a4380233111cda3c08b377f34dbb97c
CarpinettiMatias <carpi417@gmail.com>
2021-11-05 22:15:57 -0300

    estilos a texto
    estilos

--- src/component/ItemListContainer/ItemListContainer.js
+++ src/component/ItemListContainer/ItemListContainer.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 
 const ItemListContainer = ({greeting}) => {
-
+    const estilos = {
+        margin:'10px',
+        color:'pink',
+        fontSize: '50px'
+    }
 
     return (
         <div className='greeting'>
-            <h2 className='text-center' >{greeting}</h2>
+            <h2 className='text-center' style={estilos}>{greeting}</h2>
             <hr />
         </div>
 
